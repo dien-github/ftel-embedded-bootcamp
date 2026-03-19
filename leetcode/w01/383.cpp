128ed77b1992537f13b03e4a9ebbc40a3a2b1d5a
dien-github <145799386+dien-github@users.noreply.github.com>
2026-03-19 18:01:51 +0700

    [A] Solution #383
    Add first solution for Problem #383.

---
+++ leetcode/w01/383.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        std::unordered_map<char, int> hash;
+        for (char c : magazine) {
+           hash[c]++;
+        }
+        for (char c : ransomNote) {
+            if (hash[c] == 0) {
+                return false;
+            }
+            hash[c]--;
+        }
+        return true;
+    }
+};
